72f841277fb442b5d1b41b84bf2e3acf9329c6f7
fathan.hadyan <fathan.hadyan@ui.ac.id>
2024-09-06 08:13:57 +0700

    first commit

---
+++ manpromanpro/src/test/java/apap/tutorial/manpromanpro/ManpromanproApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tutorial.manpromanpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManpromanproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
